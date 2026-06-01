17de22ff03abfe3d2031f69a0ae9bca200a5e98c
palkovics adam <pa.adam94@gmail.com>
2026-06-02 00:11:33 +0200

    module-info fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module io.github.zoldseges {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens io.github.zoldseges to javafx.fxml;
     exports io.github.zoldseges;
+    exports io.github.zoldseges.ui;
+
+    opens io.github.zoldseges to javafx.fxml;
+    opens io.github.zoldseges.ui to javafx.fxml;
 }
